951c94416e09b0e1bf758eac12ffb7ab572930ea
AlushevSergei <128788339+AlushevSergei@users.noreply.github.com>
2023-08-08 12:24:20 +0500

    Create test_dag.py

---
+++ test_dag.py
@@ -0,0 +1,25 @@
+from airflow import DAG
+from airflow.operators import PythonOperator #Функция которая позволяет описывать task внутри DAGa
+from datatime import datatime # Позволяет прописывать время задач
+import pathlib
+
+# 2. Прописываем дефолтные аргументы в словаре
+default_args = {
+'owner': 'osilyutina',
+'depends_on_past': False,
+'start_date': datetime(2019, 2, 20),
+'retries': 0
+}
+# 3. Описываем основные метаданные DAGа - расписание, название
+dag = DAG('hello_world',
+default_args=default_args,
+schedule_interval='00 12 * * 1')
+
+# 4. Если используем PythonOperator, можем прямо здесь описать функцию,
+которая будет исполняться по расписанию
+
+def hello():
+print('Hello, world!')
+5. Описываем таску, которая будет исполнять нашу функцию hello()
+t1 = PythonOperator(task_id='task1', python_callable=hello,
+dag=dag)
